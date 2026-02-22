4a484a3b28655ce70789f4c522583292b854078a
Shravan Pandala <shravanpandala@shravan-macbook-air.local>
2026-02-23 01:13:51 +0530

    update: sync miscellaneous front-end changes

---
+++ src/pages/perks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Perks() {
+  return (
+    <section
+      id="perks"
+      className="bg-custom-black w-full py-16 md:py-24 overflow-hidden"
+    >
+      {/* Perks Image (Cards) */}
+      <div className="flex flex-col mt-4 md:mt-8">
+        <img
+          src="/assets/perks/Perks.png"
+          alt="Perks"
+          className="w-full h-auto"
+        />
+      </div>
+    </section>
+  );
+}
